9ef6e1b1f2181b46c67fb178e8bc8e860a5eed86
t16cs040 <t16cs040@yamanashi.ac.jp>
2018-10-24 13:02:43 +0900

    はじめてのDjangoアプリ作成 その5 完了

--- polls/urls.py
+++ polls/urls.py
@@ -11,12 +11,7 @@ app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    # added the word 'specifics'
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
 ]
